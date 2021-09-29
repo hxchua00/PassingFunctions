f90274eb6f1bfd9d55708d28ae562092b29a7c9e
hxchua00 <hxchua00@gmail.com>
2021-09-29 14:49:30 +0800

    Add project files.

---
+++ PassingFunctions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace PassingFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeFunc(PassFunc);
+            Console.ReadLine();
+        }
+        static int PassFunc(int x)
+        {
+            return x * x;
+        }
+        static void SomeFunc(Func<int, int> PassFunc)
+        {
+            int i = PassFunc(10);
+            Console.WriteLine("i = {0}", i);
+            Console.WriteLine("Function to be executed is: {0}", GetCurrentMethodName());
+        }
+        public static string GetCurrentMethodName()
+        {
+            StackTrace stackTrace = new StackTrace();
+            StackFrame stackFrame = stackTrace.GetFrame(1);
+
+            return stackFrame.GetMethod().Name;
+        }
+    }
+}
